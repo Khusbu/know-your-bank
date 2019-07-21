8931034bbe4a00d149169eaaa612a8a24aa6c970
Khusbu Mishra <meetkhusbumishra@gmail.com>
2019-07-21 18:08:30 +0530

    Add postgresql setup

--- app.py
+++ app.py
@@ -1,8 +1,23 @@
-from flask import Flask, request
+import os
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config.from_object(os.environ['APP_SETTINGS'])
+db = SQLAlchemy(app)
 
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+
+@app.route("/get_first")
+def get_first():
+    from models import Bank
+    return jsonify(Bank.query.first().serialize())
+
+
+if __name__ == '__main__':
+    app.run()
